f2f6c2c154a9b45befb0341bfcf81dc21c3b6517
Teely Tao <shabaz6105@gmail.com>
2024-08-06 10:25:34 -0400

    Add config.fish file

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+export PATH=$PATH:/home/teely/.local/bin
+export PATH=$PATH:/usr/bin
+alias config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+oh-my-posh init fish --config ~/paradox.omp.json | source
+
